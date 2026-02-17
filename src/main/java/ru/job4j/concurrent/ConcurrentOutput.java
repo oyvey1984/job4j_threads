6f8d0e851d826acca44ccd85130adcb156195fb9
Oy vey <oyiveyi@gmail.com>
2026-02-17 20:27:39 +0300

    2. Создание и запуск нити [#1016]

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -4,9 +4,14 @@ public class ConcurrentOutput {
     public static void main(String[] args) {
         System.out.println("Before start: " + Thread.currentThread().getName());
         Thread another = new Thread(
-                () -> System.out.println("Inside thread: " + Thread.currentThread().getName())
+                () -> System.out.println("Inside thread another: " + Thread.currentThread().getName())
         );
         another.start();
+
+        Thread second = new Thread(
+                () -> System.out.println("Inside thread second: " + Thread.currentThread().getName())
+        );
+        second.start();
         System.out.println("After start: " + Thread.currentThread().getName());
     }
 }
