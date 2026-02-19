55147703d0c868169163dba3974b0b0502f9d35d
Oy vey <oyiveyi@gmail.com>
2026-02-19 23:24:41 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+import java.util.concurrent.TransferQueue;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println("Name first thread: " + Thread.currentThread().getName())
+        );
+        System.out.println(first.getState());
+        first.start();
+
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+        }
+        System.out.println(first.getState());
+
+        Thread second = new Thread(
+                () -> System.out.println("Name second thread: " + Thread.currentThread().getName())
+        );
+        second.start();
+    }
+}
