ebbeb801701124d56c8f54333b320a209a499a24
Oy vey <oyiveyi@gmail.com>
2026-02-23 23:56:50 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                var process = new char[] {'-', '\\', '|', '/'};
+                if (index == 4) {
+                    index = 0;
+                }
+                System.out.print("\r Loading ... " + process[index]);
+                index++;
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
