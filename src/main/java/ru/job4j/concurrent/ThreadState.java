f407c76fe91e73bd14b7bc4dcd704f9aa58be874
Oy vey <oyiveyi@gmail.com>
2026-02-24 22:20:25 +0300

    3. Состояние нити. [#229175 #558525]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -6,25 +6,23 @@ public class ThreadState {
         Thread first = new Thread(
                 () -> System.out.println("NAME first thread: " + Thread.currentThread().getName())
         );
-        System.out.println("State first thread BEFORE cycle: " + first.getState());
-        first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println("State first thread IN cycle: " + first.getState());
-        }
-        System.out.println("State first thread AFTER cycle: " + first.getState());
-
         Thread second = new Thread(
                 () -> System.out.println("NAME SECOND thread: " + Thread.currentThread().getName())
         );
+
+        System.out.println("State first thread BEFORE cycle: " + first.getState());
         System.out.println("State SECOND thread BEFORE cycle: " + second.getState());
+
+        first.start();
         second.start();
-        while (second.getState() != Thread.State.TERMINATED) {
+
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("State first thread IN cycle: " + first.getState());
             System.out.println("State SECOND thread IN cycle: " + second.getState());
         }
-        System.out.println("State SECOND thread AFTER cycle: " + second.getState());
 
-        if (first.getState() == Thread.State.TERMINATED && second.getState() == Thread.State.TERMINATED) {
-            System.out.println("Main: work i done");
-        }
+        System.out.println("State first thread AFTER cycle: " + first.getState());
+        System.out.println("State SECOND thread AFTER cycle: " + second.getState());
+        System.out.println("Main: work is done");
     }
 }
